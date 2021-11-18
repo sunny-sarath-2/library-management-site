8809d80820407dcb5c2fc61f8715bf89b5fc81c0
sunny <sunny@ladigital.me>
2021-11-17 19:28:29 -0500

    stage-1 updated ip address

--- src/services/serviceBase.js
+++ src/services/serviceBase.js
@@ -36,7 +36,7 @@ function getHeaders(url) {
 }
 
 function getUrl(url) {
-  let base_url = "http://localhost:3001";
+  let base_url = "http://172.31.46.133:3001";
   //noinspection JSUnresolvedVariable
   return `${base_url}${url}`;
 }
